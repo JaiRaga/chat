1fe06014924a9f188802a5baee76ec785c1dd637
JaiRaga <raga.j.santhosh@gmail.com>
2021-05-26 17:24:36 +0530

    connected with socketio

--- server/index.js
+++ server/index.js
@@ -1,10 +1,23 @@
+const path = require('path')
 const express = require('express')
+const socketio = require('socket.io')
+const http = require('http')
 const cors = require('cors')
 const app = express()
 
 app.use(express.json())
 app.use(cors())
 
+const server = http.createServer(app)
+const io = socketio(server)
+
+const pathtoPublicDir = path.join(__dirname, 'public')
+app.use(express.static(pathtoPublicDir))
+
+io.on('connection', () => {
+	console.log('New Socket Connection!')
+})
+
 const PORT = process.env.PORT || 9000
 
-app.listen(PORT, () => console.log(`Server is up on ${PORT}`))
+server.listen(PORT, () => console.log(`Server is up on ${PORT}`))
