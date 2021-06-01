bd8bd4457dc04503b5523326f0dfbbed7c0e0740
JaiRaga <raga.j.santhosh@gmail.com>
2021-06-01 17:50:58 +0530

    setup message snding and receiving sockets and added filter for messages

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const socketio = require('socket.io')
 const http = require('http')
 const cors = require('cors')
+const Filter = require('bad-words')
 const app = express()
 
 app.use(express.json())
@@ -14,8 +15,50 @@ const io = socketio(server)
 const pathtoPublicDir = path.join(__dirname, 'public')
 app.use(express.static(pathtoPublicDir))
 
-io.on('connection', () => {
+// let count = 0
+io.on('connection', (socket) => {
 	console.log('New Socket Connection!')
+
+	socket.emit('message', 'Welcome!')
+
+	socket.broadcast.emit('message', 'A User has entered the chat.')
+
+	// Sending messages
+	socket.on('sendMessage', (message, callback) => {
+		const filter = new Filter()
+		if (filter.isProfane(message)) {
+			return callback('Profanity is not allowed!')
+		}
+
+		console.log(message)
+
+		io.emit('message', message)
+
+		callback('Delivered!')
+	})
+
+	socket.on('disconnect', () => {
+		io.emit('message', 'A User has left the chat.')
+	})
+
+	// Sending locations
+	socket.on('sendLocation', (location, cb) => {
+		const { latitude, longitude } = location
+		socket.broadcast.emit(
+			'message',
+			`https://google.com/maps?q=${latitude},${longitude}`
+		)
+		cb('Location Shared!')
+	})
+
+	// socket.emit('message', message)
+
+	// socket.emit('countUpdated', count)
+
+	// socket.on('increment', () => {
+	// 	++count
+	// 	io.emit('countUpdated', count)
+	// })
 })
 
 const PORT = process.env.PORT || 9000
