d45b0f452c5119957ab525d1800dad1b4a5e5770
JaiRaga <raga.j.santhosh@gmail.com>
2021-06-01 17:52:30 +0530

    setup message sending and location sharing through web sockets

--- server/public/js/chat.js
+++ server/public/js/chat.js
@@ -1,1 +1,43 @@
-io()
+const socket = io()
+
+document.querySelector('#message-form').addEventListener('submit', (e) => {
+	e.preventDefault()
+
+	const message = e.target.elements.message.value
+
+	socket.emit('sendMessage', message, (error) => {
+		if (error) {
+			return console.log(error)
+		}
+		console.log('Message Delivered!')
+	})
+})
+
+socket.on('message', (message) => console.log(message))
+
+document.querySelector('#send-location').addEventListener('click', () => {
+	if (!navigator.geolocation) {
+		return
+	}
+	navigator.geolocation.getCurrentPosition((position) => {
+		const { latitude, longitude } = position.coords
+		const location = { latitude, longitude }
+
+		socket.emit('sendLocation', location, (message) => {
+			console.log(message)
+		})
+	})
+})
+
+// socket.on('countUpdated', (count) => {
+// 	console.log(count)
+// })
+
+// document.querySelector('#increment').addEventListener('click', () => {
+// 	console.log('Clicked!')
+// 	socket.emit('increment')
+// })
+
+// socket.on('message', (message) => {
+// 	console.log(message)
+// })
